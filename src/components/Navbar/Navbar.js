b19a5a22a1040270dc9223a8ae6d99829df53d35
Manuel <manuelpinedacabeza@gmail.com>
2023-03-01 11:46:34 +0100

    Updated Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,46 +1,47 @@
-import React from "react";
+import React from 'react'
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { mainNavbarItems } from './consts/navbarItems';
+import { navbarStyles } from './styles';
+import { useNavigate } from "react-router-dom";
 
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+const Navbar = () => {
+    const navigate = useNavigate();
 
-import { mainNavbarItems } from "./consts/navbarItems";
-import { navbarStyles } from "./styles";
-import { useParams, useNavigate } from "react-router-dom";
-
-export const Navbar = () => {
-  const navigate = useNavigate();
-
-  return (
-    <Drawer sx={navbarStyles.drawer} variant="permanent" anchor="left">
-      <Toolbar />
-      <Divider />
-      <List>
-        {mainNavbarItems.map((item, index) => (
-          <ListItem
-            key={item.id}
-            onClick={() => navigate(item.route)}
-            disablePadding
-          >
-            <ListItemButton>
-              <ListItemIcon sx={navbarStyles.icons}>{item.icon}</ListItemIcon>
-              <ListItemText sx={navbarStyles.text} primary={item.label} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-    </Drawer>
-  );
+    return (
+        <Drawer
+          sx={navbarStyles.drawer}
+          variant="permanent"
+          anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {mainNavbarItems.map((item, index) => (
+            <ListItem
+                button
+                key={item.id}
+                onClick={() => navigate(item.route)}
+            >
+              <ListItemIcon
+                sx={navbarStyles.icons}
+              >
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText
+                sx={navbarStyles.text}
+                primary={item.label}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    );
 };
+
+export default Navbar
