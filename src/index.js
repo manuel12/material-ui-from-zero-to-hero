819f4c01b91a4ba629e5fbbca59b0a902cc0531d
Manuel <manuelpinedacabeza@gmail.com>
2023-03-01 11:46:56 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -7,24 +7,27 @@ import Functions from "./pages/Functions/Functions";
 import Hosting from "./pages/Hosting/Hosting";
 import MachineLearning from "./pages/MachineLearning/MachineLearning";
 import Storage from "./pages/Storage/Storage";
-
 import App from "./App";
+
+import { ThemeProvider } from "@emotion/react";
+import { dashboardTheme } from "./dashboardTheme";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  // <App />
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="authentication" element={<Authentication />} />
-        <Route path="database" element={<Database />} />
-        <Route path="functions" element={<Functions />} />
-        <Route path="hosting" element={<Hosting />} />
-        <Route path="machine-learning" element={<MachineLearning />} />
-        <Route path="storage" element={<Storage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <ThemeProvider theme={dashboardTheme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="authentication" element={<Authentication />} />
+          <Route path="database" element={<Database />} />
+          <Route path="functions" element={<Functions />} />
+          <Route path="hosting" element={<Hosting />} />
+          <Route path="machine-learning" element={<MachineLearning />} />
+          <Route path="storage" element={<Storage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
