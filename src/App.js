a142857fedb986ae8620cfc33cfc6f87c565f05a
Manuel <manuelpinedacabeza@gmail.com>
2023-03-01 11:46:16 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
-import { Navbar } from "./components/Navbar/Navbar";
 import Grid from "@mui/material/Grid";
+import Header from "./components/Header/Header";
+import { useLocation } from "react-router-dom";
+import { Button } from "@mui/material";
 
-export default function App() {
+function App() {
+  const [title, setTitle] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    const parsedTitle = location.pathname.replace(/\W/g, " ");
+    setTitle(parsedTitle);
+  }, [location]);
+
   return (
     <Grid container>
       <Navbar />
+      <Header title={title} />
       <Outlet />
     </Grid>
   );
 }
+
+export default App;
