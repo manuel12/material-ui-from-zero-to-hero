311e4e3cede45c4e09af6ce05b63f4f1d2d35917
Manuel <manuelpinedacabeza@gmail.com>
2023-03-01 11:47:16 +0100

    Updated Authentication.js

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,14 +1,67 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Grid, IconButton, Typography } from "@mui/material";
+import BasicCard from "../../components/common/BasicCard/BasicCard";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import SearchBar from "../../components/common/SearchBar/SearchBar";
+import CommonButtton from "../../components/common/CommonButton/CommonButton";
+import GridWrapper from "../../components/common/GridWrapper/GridWrapper";
+import { cardHeaderStyles } from "./styles";
 
 const Authentication = () => {
+  const getHeader = () => {
+    const handleChange = (value) => {
+      console.log(value);
+    };
+
+    const addUser = () => {
+      console.log("Click!");
+    };
+
+    return (
+      <Box sx={cardHeaderStyles.wrapper}>
+        <SearchBar
+          placeholder="Search by email address, phone number, or user UID"
+          onChange={(event) => handleChange(event.target.value)}
+          searchBarWidth="720px"
+        />
+        <Box>
+          <CommonButtton
+            variant="contained"
+            onClick={addUser}
+            size="large"
+            sx={cardHeaderStyles.addUserButton}
+          >
+            Add user
+          </CommonButtton>
+          <IconButton>
+            <RefreshIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    );
+  };
+
+  const getContent = () => {
+    return (
+      <>
+        <Typography
+          align="center"
+          sx={{
+            margin: "40px 16px",
+            color: "rgba(0,0,0,0.6)",
+            fontSize: "1.3rem",
+          }}
+        >
+          No users for this project yet.
+        </Typography>
+      </>
+    );
+  };
+
   return (
-    <Grid
-      item
-      xs={8}
-    >
-      Authentication
-    </Grid>
+    <GridWrapper>
+      <BasicCard header={getHeader()} content={getContent()} />
+    </GridWrapper>
   );
 };
 
