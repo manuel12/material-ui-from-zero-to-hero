b9fbf34576c1fe5fd39a03fd2b500a276e08eebd
Manuel <manuelpinedacabeza@gmail.com>
2023-03-03 13:46:42 +0100

    Create BasicSnackbar component.

---
+++ src/components/common/BasicSnackbar/BasicSnackbar.js
@@ -0,0 +1,28 @@
+import React, { forwardRef } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import CommonButton from "../../common/CommonButton/CommonButton";
+
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const BasicSnackbar = ({ open, onClose, severity, message }) => {
+  return (
+    <>
+      <Snackbar 
+        open={open} 
+        autoHideDuration={6000} 
+        onClose={onClose}>
+        <Alert 
+          onClose={onClose} 
+          severity={severity} 
+          sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </>
+  ); 
+};
+
+export default BasicSnackbar;
